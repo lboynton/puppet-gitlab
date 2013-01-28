e0e54773dd885f0d84e4a11280a3c73d7853ad8a
Lee Boynton <lee@lboynton.com>
2013-01-28 13:49:34 +0000

    Run bundle install command

--- manifests/gitlab.pp
+++ manifests/gitlab.pp
@@ -26,4 +26,34 @@ class gitlab::gitlab(
         group       => 'gitlab',
         content     => template('gitlab/database.yml.erb'),
     }
+
+    if !defined(Package['mysql-devel']) {
+        package {'mysql-devel':
+            ensure  => installed,
+        }
+    }
+
+    if !defined(Package['libicu-devel']) {
+        package { 'libicu-devel':
+            ensure      => installed,
+        }
+    }
+
+    package { 'charlock_holmes':
+        ensure      => installed,
+        provider    => gem,
+        require     => Package['libicu-devel'],
+    }
+
+    exec { '/usr/local/rvm/gems/ruby-1.9.3-p374@global/bin/bundle install --deployment --without development test postgres':
+        cwd         => '/home/gitlab/gitlab',
+        user        => 'gitlab',
+        require     => [
+            Class['gitlab::ruby'], 
+            Vcsrepo['gitlab'], 
+            Package['charlock_holmes'], 
+            Package['mysql-devel']
+        ],
+        logoutput   => on_failure,
+    }
 }
